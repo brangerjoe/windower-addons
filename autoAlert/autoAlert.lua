d0a97cefb9cc118604059e29c887a37220ce5b19
brangerjoe <brangerjoe@gmail.com>
2019-02-28 11:12:42 -0800

    Refactor and clean up

--- autoAlert/autoAlert.lua
+++ autoAlert/autoAlert.lua
@@ -1,49 +1,32 @@
 _addon.name = "autoAlert"
 _addon.author = "Godchain"
-_addon.version = "1.0"
+_addon.version = "1.1"
 _addon.commands = {"autoAlert", "aa"}
 
 config = require("config")
 texts = require("texts")
 res = require("resources")
-packets = require("packets")
-alertBox = "alertBox"
-alertBox_MA = "alertBox_MA"
+
+background_ability = "background_ability"
+background_magic = "background_magic"
+background_interrupt = "background_interrupt"
 playerResolution = T {}
 playerResolution.x = windower.get_windower_settings().x_res
 playerResolution.y = windower.get_windower_settings().y_res
---settings = config.load({})
-default = {}
-default.Show = {
-    burst = _static,
-    pet = S {"BST", "SMN"},
-    props = _static,
-    spell = S {"SCH", "BLU"},
-    step = _static,
-    timer = _static,
-    weapon = _static
-}
 caption = texts.new({})
-
 showing = false
-trigger_time = -1
+last_trigger = -1
+trigger_duration = 3
+weapon_skill_category = 7
+magic_category = 8
+interrupt_id = 28787
 
 windower.register_event(
     "load",
     function()
+        create_backgrounds((playerResolution.x / 2) - 250, 50)
         caption:bg_visible(false)
         caption:bold(true)
-        windower.prim.create(alertBox)
-        windower.prim.set_fit_to_texture(alertBox, true)
-        windower.prim.set_texture(alertBox, windower.addon_path .. "images/background.png")
-        windower.prim.set_position(alertBox, (playerResolution.x / 2) - 250, 50)
-        windower.prim.set_visibility(alertBox, false)
-
-        windower.prim.create(alertBox_MA)
-        windower.prim.set_fit_to_texture(alertBox_MA, true)
-        windower.prim.set_texture(alertBox_MA, windower.addon_path .. "images/background_magic.png")
-        windower.prim.set_position(alertBox_MA, (playerResolution.x / 2) - 250, 50)
-        windower.prim.set_visibility(alertBox_MA, false)
     end
 )
 
@@ -53,7 +36,7 @@ windower.register_event(
         if showing then
             local x, y = caption:extents()
             caption:pos(playerResolution.x / 2 - x / 2, 53)
-            if os.time() - trigger_time > 3 then
+            if os.time() - last_trigger > trigger_duration then
                 hide_caption()
             end
         else
@@ -82,41 +65,75 @@ windower.register_event(
             return
         end
 
-        if act.category == 7 and act.actor_id == target then
+        if act.category == weapon_skill_category and act.actor_id == target then
             local skill_name =
                 res.monster_abilities[act.targets[1].actions[1].param] and
                 res.monster_abilities[act.targets[1].actions[1].param].name or
                 "???"
-            local caption_display = act.param == 28787 and "Interrupted!" or skill_name
-            show_caption(caption_display, "ws")
-        elseif act.category == 8 and act.actor_id == target then
+
+            if act.param == interrupt_id then
+                skill_name = "Interrupted!"
+                show_caption(skill_name, "int")
+            else
+                show_caption(skill_name, "ws")
+            end
+        elseif act.category == magic_category and act.actor_id == target then
             local spell_name =
                 res.spells[act.targets[1].actions[1].param] and res.spells[act.targets[1].actions[1].param].name or
                 "???"
 
-            local caption_display = act.param == 28787 and "Interrupted!" or spell_name
-            show_caption(caption_display, "ma")
+            if act.param == interrupt_id then
+                skill_name = "Interrupted!"
+                show_caption(skill_name, "int")
+            else
+                show_caption(skill_name, "ma")
+            end
         end
     end
 )
+
+function create_backgrounds(x, y)
+    windower.prim.create(background_ability)
+    windower.prim.set_fit_to_texture(background_ability, true)
+    windower.prim.set_texture(background_ability, windower.addon_path .. "images/background_ability.png")
+    windower.prim.set_position(background_ability, x, y)
+    windower.prim.set_visibility(background_ability, false)
+
+    windower.prim.create(background_magic)
+    windower.prim.set_fit_to_texture(background_magic, true)
+    windower.prim.set_texture(background_magic, windower.addon_path .. "images/background_magic.png")
+    windower.prim.set_position(background_magic, x, y)
+    windower.prim.set_visibility(background_magic, false)
+
+    windower.prim.create(background_interrupt)
+    windower.prim.set_fit_to_texture(background_interrupt, true)
+    windower.prim.set_texture(background_interrupt, windower.addon_path .. "images/background_interrupt.png")
+    windower.prim.set_position(background_interrupt, x, y)
+    windower.prim.set_visibility(background_interrupt, false)
+end
 
 function show_caption(text, type)
     showing = true
     caption:text(text)
     caption:show()
-    windower.play_sound(windower.addon_path .. "sounds/alert.wav")
 
     if (type == "ws") then
-        windower.prim.set_visibility(alertBox, true)
+        windower.play_sound(windower.addon_path .. "sounds/ability_alert.wav")
+        windower.prim.set_visibility(background_ability, true)
     elseif (type == "ma") then
-        windower.prim.set_visibility(alertBox_MA, true)
+        windower.play_sound(windower.addon_path .. "sounds/magic_alert.wav")
+        windower.prim.set_visibility(background_magic, true)
+    elseif (type == "int") then
+        windower.play_sound(windower.addon_path .. "sounds/interrupt_alert.wav")
+        windower.prim.set_visibility(background_interrupt, true)
     end
-    trigger_time = os.time()
+    last_trigger = os.time()
 end
 
 function hide_caption()
     showing = false
     caption:hide()
-    windower.prim.set_visibility(alertBox, false)
-    windower.prim.set_visibility(alertBox_MA, false)
+    windower.prim.set_visibility(background_ability, false)
+    windower.prim.set_visibility(background_magic, false)
+    windower.prim.set_visibility(background_interrupt, false)
 end
